c2db036969e98e92b165c54f95ac5c7131eefcd3
Jeff Standen <jeff@webgroupmedia.com>
2014-01-07 03:51:06 -0800

    * [Plugins/S3 Gatekeeper] The S3 Gatekeeper storage engine can now provide 'Bucket' and 'Path Prefix' as configuration parameters.  Previously these were returned by the external authentication, but some S3 requests (like multiple delete) need to know them in advance in order to sign the request.
    * [Plugins/S3 Gatekeeper] When configuring the S3 Gatekeeper storage engine, the tester will now PUT, GET, and DELETE a temporary file in order to verify that the credentials work for all the necessary actions.
    
    * [Plugin/S3 Gatekeeper] Implemented batch delete functionality in the S3 Gatekeeper storage engine.

--- api/App.php
+++ api/App.php
@@ -21,13 +21,49 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 		@$username = DevblocksPlatform::importGPC($_POST['username'],'string','');
 		@$password = DevblocksPlatform::importGPC($_POST['password'],'string','');
 		@$url = DevblocksPlatform::importGPC($_POST['url'],'string','');
+		@$bucket = DevblocksPlatform::importGPC($_POST['bucket'],'string','');
+		@$path_prefix = DevblocksPlatform::importGPC($_POST['path_prefix'],'string','');
 		
-		if(!isset($username) || !isset($password) || !isset($url))
-			return false;
-		
-		if(!$this->_getSignedURL($username, $password, $url . '?test=true'))
+		if(empty($url) || empty($username) || empty($password))
 			return false;
 
+		$path_prefix =
+			0 == strlen(trim($path_prefix, '/'))
+			? ''
+			: (trim($path_prefix, '/') . '/')
+			;
+		
+		$key = $path_prefix . '.cerb_test';
+
+		// PUT
+		
+		$headers = array(
+			//'Date' => gmdate(DATE_RFC2822),
+			'Content-Type' => 'text/plain',
+		);
+		
+		if(false == ($signed_url = $this->_getSignedURL($username, $password, $url, 'PUT', $key, $headers)))
+			return false;
+
+		if(false == $this->_execute($signed_url, 'PUT', 'CERB', $headers))
+			return false;
+
+		// GET
+		
+		if(false == ($signed_url = $this->_getSignedURL($username, $password, $url, 'GET', $key)))
+			return false;
+
+		if(false == $this->_execute($signed_url, 'GET'))
+			return false;
+		
+		// DELETE
+		
+		if(false == ($signed_url = $this->_getSignedURL($username, $password, $url, 'DELETE', $key)))
+			return false;
+
+		if(false == $this->_execute($signed_url, 'DELETE'))
+			return false;
+		
 		return true;
 	}
 
@@ -42,12 +78,22 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 		@$username = DevblocksPlatform::importGPC($_POST['username'],'string','');
 		@$password = DevblocksPlatform::importGPC($_POST['password'],'string','');
 		@$url = DevblocksPlatform::importGPC($_POST['url'], 'string', '');
+		@$bucket = DevblocksPlatform::importGPC($_POST['bucket'], 'string', '');
+		@$path_prefix = DevblocksPlatform::importGPC($_POST['path_prefix'], 'string', '');
 
+		$path_prefix =
+			0 == strlen(trim($path_prefix, '/'))
+			? ''
+			: (trim($path_prefix, '/') . '/')
+			;
+		
 		$fields = array(
 		DAO_DevblocksStorageProfile::PARAMS_JSON => json_encode(array(
 				'username' => $username,
 				'password' => $password,
-				'url' => $url
+				'url' => $url,
+				'bucket' => $bucket,
+				'path_prefix' => $path_prefix,
 			)),
 		);
 
@@ -55,7 +101,8 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 	}
 
 	public function exists($namespace, $key) {
-		$path = $this->escapeNamespace($namespace) . '/' . $key;
+		//@$bucket = $this->_options['bucket'];
+		$path = $this->_options['path_prefix'] . $this->escapeNamespace($namespace) . '/' . $key;
 		
 		if(false == ($url = $this->_getSignedURL($this->_options['username'], $this->_options['password'], $this->_options['url'], 'GET', $path)))
 			return false;
@@ -64,27 +111,36 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 	}
 	
 	public function put($namespace, $id, $data, $length = null) {
+		//@$bucket = $this->_options['bucket'];
+		
 		// Get a unique hash path for this namespace+id
 		$hash = base_convert(sha1($this->escapeNamespace($namespace).$id), 16, 32);
+		
 		$key = sprintf("%s/%s/%d",
 			substr($hash,0,1),
 			substr($hash,1,1),
 			$id
 		);
 		
-		$path = $this->escapeNamespace($namespace) . '/' . $key;
+		// [TODO] This should store the real Content-Type
+		$headers = array(
+			'Content-Type' => 'application/octet-stream',
+		);
+		
+		$path = $this->_options['path_prefix'] . $this->escapeNamespace($namespace) . '/' . $key;
 	
-		if(false == ($url = $this->_getSignedURL($this->_options['username'], $this->_options['password'], $this->_options['url'], 'PUT', $path)))
+		if(false == ($url = $this->_getSignedURL($this->_options['username'], $this->_options['password'], $this->_options['url'], 'PUT', $path, $headers)))
 			return false;
 		
-		if(false == ($this->_execute($url, 'PUT', $data, $length)))
+		if(false == ($this->_execute($url, 'PUT', $data, $headers)))
 			return false;
 	
 		return $key;
 	}
 	
 	public function get($namespace, $key, &$fp=null) {
-		$path = $this->escapeNamespace($namespace) . '/' . $key;
+		//@$bucket = $this->_options['bucket'];
+		$path = $this->_options['path_prefix'] . $this->escapeNamespace($namespace) . '/' . $key;
 		
 		if(false == ($url = $this->_getSignedURL($this->_options['username'], $this->_options['password'], $this->_options['url'], 'GET', $path)))
 			return false;
@@ -111,28 +167,114 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 	}
 	
 	public function delete($namespace, $key) {
-		// [TODO] Fail gracefully if resource doesn't exist (pass)
-		$path = $this->escapeNamespace($namespace) . '/' . $key;
+		//@$bucket = $this->_options['bucket'];
+		//$path = $this->_options['path_prefix'] . $this->escapeNamespace($namespace) . '/' . $key;
 	
+		/*
+		// [TODO] Fail gracefully if resource doesn't exist (pass)
 		if(false ==  ($url = $this->_getSignedURL($this->_options['username'], $this->_options['password'], $this->_options['url'], 'DELETE', $path)))
 			return false;
 	
 		if(false == ($data = $this->_execute($url, 'DELETE')))
 			return false;
+		*/
+		
+		// Queue up batch DELETEs
+		$profile_id = isset($this->_options['_profile_id']) ? $this->_options['_profile_id'] : 0;
+		DAO_DevblocksStorageQueue::enqueueDelete($namespace, $key, $this->manifest->id, $profile_id);
 	
 		return TRUE;
 	}
+	
+	public function batchDelete($namespace, $keys) {
+		@$bucket = $this->_options['bucket'];
+		$ns = $this->escapeNamespace($namespace);
+		$path_prefix = $this->_options['path_prefix'];
+		
+		$paths = array_map(function($e) use ($ns, $bucket, $path_prefix) {
+			return $path_prefix . $ns . '/' . $e;
+		}, $keys);
 
-	private function _getSignedURL($username, $password, $url, $verb = 'GET', $filename = null) {
+		$dom = new DOMDocument();
+		$dom->formatOutput = true;
+		
+		$node_delete = $dom->createElement('Delete');
+		
+		$node_quiet = $dom->createElement('Quiet', 'true');
+		$node_delete->appendChild($node_quiet);
+		
+		if(is_array($paths))
+		foreach($paths as $path) {
+			$node_object = $dom->createElement('Object');
+			
+			$node_key = $dom->createElement('Key', $path);
+			$node_object->appendChild($node_key);
+			
+			$node_delete->appendChild($node_object);
+		}
+		
+		$dom->appendChild($node_delete);
+		
+		$data = $dom->saveXML();
+		
+		$http_date = gmdate('D, d M Y H:i:s') . ' GMT';
+		
+		$headers = array(
+			'Content-MD5' => base64_encode(md5($data, true)),
+			'Content-Type' => 'application/xml',
+			'Date' => $http_date,
+		);
+		
+		if(false == ($url = $this->_getSignedURL($this->_options['username'], $this->_options['password'], $this->_options['url'], 'POST', '?delete', $headers)))
+			return false;
+
+		$parsed = parse_url($url);
+		$query_str = $parsed['query'];
+		parse_str($query_str, $query);
+		
+		$headers['Authorization'] = sprintf("AWS %s:%s",
+			$query['AWSAccessKeyId'],
+			$query['Signature']
+		);
+		
+		$url = sprintf("http://s3.amazonaws.com/%s/?delete",
+			$bucket
+		);
+		
+		if(false === ($result = $this->_execute($url, 'POST', $data, $headers)))
+			return false;
+		
+		// Handle the case where some fail to delete (e.g. AccessDenied)
+		
+		$xml = simplexml_load_string($result);
+		
+		$errors = array();
+		
+		if(isset($xml->Error))
+		foreach($xml->Error as $error) {
+			$errors[] = str_replace($path_prefix . $ns . '/', '', $error->Key);
+		}
+		
+		// Return the keys that were actually deleted, ignoring any errors
+		return array_diff($keys, $errors);
+	}
+	
+	private function _getSignedURL($username, $password, $url, $verb = 'GET', $key = null, $headers = array()) {
 		$logger = DevblocksPlatform::getConsoleLog();
 		
 		$header = array();
 		$ch = curl_init();
 		
-		$payload = array('verb' => $verb, 'key' => $filename);
-		$http_date = gmdate(DATE_RFC822);
+		if(!isset($headers['Date']))
+			$headers['Date'] = gmdate('D, d M Y H:i:s') . ' GMT';
 		
-		$header[] = 'Date: '.$http_date;
+		$payload = array('json' => json_encode(array(
+			'verb' => $verb,
+			'key' => $key,
+			'headers' => $headers,
+		)));
+		
+		$header[] = 'Date: ' . $headers['Date'];
 		$header[] = 'Content-Type: application/x-www-form-urlencoded; charset=utf-8';
 		
 		$postfields = '';
@@ -143,6 +285,7 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 					$postfields .= $key.'='.rawurlencode($value) . '&';
 				}
 				rtrim($postfields,'&');
+				
 			} elseif (is_string($payload)) {
 				$postfields = $payload;
 			}
@@ -163,9 +306,16 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 		$secret = strtolower(md5($password));
 		
 		// Hardcoded as POST because we will only ever POST to the gatekeeper script
-		$string_to_sign = "POST\n$http_date\n$url_path\n$url_query\n$postfields\n$secret\n";
+		$string_to_sign = sprintf("%s\n%s\n%s\n%s\n%s\n%s\n",
+			'POST',
+			$headers['Date'],
+			$url_path,
+			$url_query,
+			$postfields,
+			$secret
+		);
 		$hash = md5($string_to_sign); // base64_encode(sha1(
-		$header[] = 'Cerb5-Auth: '.sprintf("%s:%s",$username,$hash);
+		$header[] = 'Cerb-Auth: '.sprintf("%s:%s",$username,$hash);
 		
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
@@ -188,24 +338,31 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 		if(!is_array($output) || empty($output) || $output['__status'] == 'error') {
 			$logger->error(sprintf('Error connecting to Gatekeeper: %s', $output['message']));
 			return false;
+			
 		} else {
 			$output = $output['message'];
 		}
+		
 		return $output;
 	}
 	
 	// [TODO] Make this streaming safe
-	private function _execute($url, $verb = 'GET', $data = null, $length = null) {
+	private function _execute($url, $verb = 'GET', $data = null, $headers = array()) {
 		$logger = DevblocksPlatform::getConsoleLog();
+		
 		try {
 			$ch = curl_init($url);
-			$http_date = gmdate(DATE_RFC822);
+			$http_date = gmdate(DATE_RFC2822);
+			$length = null;
 	
-			//curl_setopt($ch, CURLOPT_HEADER, true);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			//curl_setopt($ch, CURLOPT_VERBOSE, true);
+			//curl_setopt($ch, CURLINFO_HEADER_OUT, true);
 	
 			if(is_resource($data)) {
-				// set class var
+				$stat = fstat($data);
+				$length = $stat['size'];
+				
 				$this->_data = $data;
 				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 				curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
@@ -214,28 +371,43 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 				curl_setopt($ch, CURLOPT_READFUNCTION, array($this, 'streamData'));
 				curl_setopt($ch, CURLOPT_UPLOAD, true);
 				curl_setopt($ch, CURLINFO_CONTENT_LENGTH_UPLOAD, $length);
+				
 			} else {
 				$length = strlen($data);
+				
 				switch($verb) {
 					case 'GET':
 						break;
+						
+					case 'POST':
 					case 'PUT':
-						curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+						curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $verb);
 						curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 						break;
+						
 					case 'DELETE':
 						curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
 						break;
 				}
 			}
 			
-			$header[] = 'Date: '.$http_date;
-			// Blank out since we pre-signed the URL
-			$header[] = 'Content-Type:';
-			$header[] = 'Content-Length: '.$length;
-			$header[] = 'Expect: ';
-			$header[] = 'Transfer-Encoding: ';
-	
+			if(isset($headers['Date']))
+				$http_date = $headers['Date'];
+			
+			$header[] = 'Date: ' . $http_date;
+
+			if(isset($headers['Authorization']))
+				$header[] = 'Authorization: ' . $headers['Authorization'];
+			
+			if(isset($headers['Content-MD5']))
+				$header[] = 'Content-MD5: ' . $headers['Content-MD5'];
+			
+			if(isset($headers['Content-Type']))
+				$header[] = 'Content-Type: ' . $headers['Content-Type'];
+			
+			if(!empty($length))
+				$header[] = 'Content-Length: '. $length;
+			
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 			$response = curl_exec($ch);
@@ -249,6 +421,7 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 			}
 	
 			curl_close($ch);
+			
 		} catch (Exception $e) {
 			return false;
 		}
@@ -256,11 +429,13 @@ class DevblocksStorageEngineGatekeeper extends Extension_DevblocksStorageEngine 
 		// Return
 		switch($verb) {
 			case 'GET':
+			case 'POST':
 				return $response;
 				break;
+				
 			default:
 				return true;
-			break;
+				break;
 		}
 	}
 	
